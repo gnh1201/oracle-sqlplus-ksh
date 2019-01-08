1754b4f32027b270278d24842153b4205d6083f9
Namhyeon, Go <gnh1201@gmail.com>
2019-01-08 17:05:38 +0900

    Update sampling.sh

--- sampling.sh
+++ sampling.sh
@@ -1,16 +1,19 @@
 #!/bin/ksh
 
 # get tables
-(echo "SELECT concat(concat(owner, '.'), table_name) as tablename FROM all_tables WHERE owner = 'ORCL';" | sqlplus -s '/as sysdba') > hello.txt
+(echo "SELECT concat(concat(owner, '.'), table_name) as tablename FROM all_tables WHERE owner = 'CERAMIC4';" | sqlplus '/as sysdba') > hello.txt
 
 file="hello.txt"
 # while loop
 while IFS= read -r line
 do
         if [[ "$line" != "TABLENAME" && "$line" != "-------------------------------------------------------------" && "$line" != "" ]];then
-            echo "$line"
-            echo "column column_name format a30;
-                set linesize 300;
-                select * from (select * from $line) t where ROWNUM < 6);"  | sqlplus -s '/as sysdba'
+            echo "@DUMPSTART"
+            echo "@TABLENAME $line"
+            echo "
+                set linesize 30000;
+                select * from (select * from $line) t where ROWNUM < 6;
+            "  | sqlplus -s '/as sysdba'
+            echo "@DUMPEND"
         fi
 done <"$file"
